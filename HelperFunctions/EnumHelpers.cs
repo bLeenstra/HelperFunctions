2f2e5f788cb25ca10a401b46c9a068ea4ae2e0b8
bLeenstra <brendan.leenstra@gmail.com>
2016-02-14 00:31:13 +1100

    EnumHelper

---
+++ HelperFunctions/EnumHelpers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperFunctions
+{
+    public class EnumHelpers
+    {
+        public static string EnumName<T>(T value) {
+            return Enum.GetName(typeof(T), value);
+        }
+    }
+}
